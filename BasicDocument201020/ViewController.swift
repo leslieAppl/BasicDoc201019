e1a5ebfbd428384dc148f070bca2833fded002ad
Leslie <leslie.appl@gmail.com>
2020-10-20 08:30:59 +0800

    Initial Commit

---
+++ BasicDocument201020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicDocument201020
+//
+//  Created by leslie on 10/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
